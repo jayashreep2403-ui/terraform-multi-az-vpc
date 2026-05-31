8a242ac14f80ec4d532109a3db1f78510e9c5bae
jayashree-patil <jayashreep2403@gmail.com>
2026-06-01 03:45:58 +0530

    Add public and private subnets in subnets.tf

---
+++ terraform/subnets.tf
@@ -0,0 +1,19 @@
+resource "aws_subnet" "public_az1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "public-subnet-az1"
+  }
+}
+
+resource "aws_subnet" "private_az1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.11.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "private-subnet-az1"
+  }
+}
